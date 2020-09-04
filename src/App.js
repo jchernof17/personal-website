957ba7bb80849940f71f84303e76f79ecd790215
jchernof17 <jordan.chernof@gmail.com>
2020-09-04 16:13:00 -0700

    remove unused vars

--- src/App.js
+++ src/App.js
@@ -4,26 +4,16 @@ import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./theme";
 import { GlobalStyles } from "./global";
 import {
-	Navbar,
-	Nav,
-	Card,
-	Button as b,
-	Jumbotron,
-	Row as r,
-} from "react-bootstrap";
-import {
 	Menu,
 	Header,
 	Container,
 	Button,
-	Icon,
 	GridRow as Row,
 	Grid,
 	Card as NewCard,
 	Image,
 	Tab,
 } from "semantic-ui-react";
-import { Tabs, Tab as OldTab } from "react-bootstrap-tabs";
 import ReactGA from "react-ga";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
@@ -52,32 +42,32 @@ const resume_link = "https://1drv.ms/b/s!AuHCf7OLnxVrirguD8zgeqRUdg9Y9g";
 const github_link = "https://github.com/jchernof17";
 const linkedin_link = "https://www.linkedin.com/in/jordan-chernof";
 const contact_me = `For internship or freelance inquiries, you can reach out to the email on my resume. Adding me on LinkedIn works as well.`;
-const extra_text = `
-You can also reach out for references.
-My name is Jordan Chernof. I am a junior at UC Berkeley studying Computer Science and Public Policy.
+// const extra_text = `
+// You can also reach out for references.
+// My name is Jordan Chernof. I am a junior at UC Berkeley studying Computer Science and Public Policy.
 
-I was born and raised in Maryland.
+// I was born and raised in Maryland.
 
-My passion is using technology to help people achieve their goals.
+// My passion is using technology to help people achieve their goals.
 
-I am a freelance developer in college, having designed webapps for several startups, nonprofwebappsits, and campus-affiliated organizations.
+// I am a freelance developer in college, having designed webapps for several startups, nonprofwebappsits, and campus-affiliated organizations.
 
-I most recently interned at Microsoft, where I built a recommendation engine for the PowerBI product. I used LP/Optimization principles to inform my work.
+// I most recently interned at Microsoft, where I built a recommendation engine for the PowerBI product. I used LP/Optimization principles to inform my work.
 
-Previously, I interned at the blockchain company ConsenSys, researching and designing Ethereum smart contracts on the Treum team.
+// Previously, I interned at the blockchain company ConsenSys, researching and designing Ethereum smart contracts on the Treum team.
 
-On campus, I help teach Public Policy 198 (Policy Review @ Berkeley), work with my freelance clients, and play IM basketball.
+// On campus, I help teach Public Policy 198 (Policy Review @ Berkeley), work with my freelance clients, and play IM basketball.
 
-`;
+// `;
 
 const bio1 = `My name is Jordan Chernof. I am a junior at UC Berkeley studying Computer Science and minoring in Public Policy.`;
 const bio2 = `I was born and raised in Maryland.`;
 const bio3 = `My passion is using technology to help people achieve their goals.`;
-const bio4 = `I am a freelance developer in college, having designed webapps for several startups, nonprofits, and campus-affiliated organizations.`;
+// const bio4 = `I am a freelance developer in college, having designed webapps for several startups, nonprofits, and campus-affiliated organizations.`;
 const consensys_bio = `Previously, I interned at the blockchain company ConsenSys, researching and designing Ethereum smart contracts on the Treum team.`;
 const microsoft_bio =
 	"I most recently interned at Microsoft, where I built a recommendation engine for Power BI. I used LP/Optimization principles to inform my work.";
-const hackathons = `I have completed three hackathons, wherein I built a startup MVP, a React club-matching app, and a clothing-design marketplace.`;
+// const hackathons = `I have completed three hackathons, wherein I built a startup MVP, a React club-matching app, and a clothing-design marketplace.`;
 const conclusion = `On campus, I help teach Public Policy 198 (Policy Review @ Berkeley), work with my freelance clients, and play IM basketball.`;
 const lighthearted = `Of course, this is a career-focused website, so I may seem a little serious. But I'm not all work! I like to read, go hiking, and make breakfast food.`;
 const directions = `A few projects are public on my Github profile, but some of the interesting work I've done is private. I'm happy to speak more about those if you're interested.`;
@@ -94,14 +84,6 @@ const allCards = [
 	MYOPCard,
 	MerkleProofCard,
 ];
-
-class AboutTab extends Component {
-	render() {
-		let label = this.props.label;
-		let text = this.props.text;
-		return <Tab.Pane style={{ "vertical-align": "center" }}>{text}</Tab.Pane>;
-	}
-}
 
 let objectToPane = (object) => {
 	return {
@@ -151,22 +133,22 @@ const paneObjects = [
 		description: directions,
 	},
 ];
-class About extends Component {
-	render() {
-		return (
-			<Row width="10">
-				<h1 style={{ color: "white" }}>About Me</h1>
-				<Tab
-					menu={{ fluid: true, vertical: true, tabular: true }}
-					panes={paneObjects.map((object) => objectToPane(object))}
-					id="bio-tabs"
-					className="primary-background content-box"
-					style={{ width: "100%", marginTop: "40px", marginBottom: "40px" }}
-				></Tab>
-			</Row>
-		);
-	}
-}
+// class About extends Component {
+// 	render() {
+// 		return (
+// 			<Row width="10">
+// 				<h1 style={{ color: "white" }}>About Me</h1>
+// 				<Tab
+// 					menu={{ fluid: true, vertical: true, tabular: true }}
+// 					panes={paneObjects.map((object) => objectToPane(object))}
+// 					id="bio-tabs"
+// 					className="primary-background content-box"
+// 					style={{ width: "100%", marginTop: "40px", marginBottom: "40px" }}
+// 				></Tab>
+// 			</Row>
+// 		);
+// 	}
+// }
 
 class NewAbout extends Component {
 	render() {
