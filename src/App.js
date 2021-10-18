8402f2ea17cb49c52e06f2098146cff2f33078b5
jchernof17 <jordan.chernof@gmail.com>
2021-10-18 16:49:00 -0700

    fix typo

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ const bio3 = `I am interested in using software to help design real-world things
 const tesla_bio = `This past Spring, I spent the semester as a Software Engineering Intern at Tesla, where I designed a load balaning portal for product teams to view and test new features before rollout.`
 const consensys_bio = `Previously, I interned at the blockchain company ConsenSys, researching and designing Ethereum smart contracts on the Treum team.`;
 const microsoft_bio =
-	"I've spent two summers as a Software Engineering Intern at Microsoft, working on Power BI. Some of my work ";
+	"I've spent two summers as a Software Engineering Intern at Microsoft, working on Power BI. Some of my work included designing a recommendation engine and building a key new product integration.";
 // const hackathons = `I have completed three hackathons, wherein I built a startup MVP, a React club-matching app, and a clothing-design marketplace.`;
 const conclusion = `On campus, I help teach Public Policy 198 (Policy Review @ Berkeley), work with my freelance clients, and play IM basketball.`;
 const lighthearted = `Of course, this is a career-focused website, so I may seem a little serious. But I'm not all work! I like to read, go hiking, and make breakfast food.`;
